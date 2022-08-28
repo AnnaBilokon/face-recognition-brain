aea14c628aee69b1ff7130814dc44298f97dfa7f
AnnaBilokon <Agnewka89@gmail.com>
2022-08-28 21:00:58 +0200

    updates

--- face-recognation-brain/src/App.js
+++ face-recognation-brain/src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Navigation} from './Navigation';
+import {Logo} from './Logo/Logo';
+import { ImageLinkForm } from './ImageLinkForm/ImageLinkForm';
+import { Rank } from './Rank/Rank';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+	render () {
+		return (
+			
+			<div className="App">
+				
+			  <Navigation/>
+			  <Logo/>
+			  <Rank/>
+			  <ImageLinkForm/>
+			   
+			  {/*<FaceRecognition/> */}
+
+			</div>
+		  );
+	}
+  
 }
 
 export default App;
